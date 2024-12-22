da0ed1bede8b648154ab906b7b47b8a0496f3140
Darryl Pogue <darryl@dpogue.ca>
2024-12-22 02:18:20 -0800

    More compatibility hacks for ancient browsers
    * Safari 5.0.6
    * Netscape Navigator 9
    * Internet Explorer for Mac 5.2.3

--- src/assets/js/ieshim.js
+++ src/assets/js/ieshim.js
@@ -3,3 +3,78 @@ document.createElement('main');
 document.createElement('article');
 document.createElement('section');
 document.createElement('footer');
+
+function fixElementsOfType(type) {
+    var blockStart, blockEnd, newBlock, elems;
+
+    elems = document.getElementsByTagName('/' + type);
+    while (elems.length) {
+        blockEnd = elems[0];
+        blockStart = blockEnd.parentNode.firstChild;
+        while (blockStart && blockStart.nodeName != type) {
+            blockStart = blockStart.nextSibling;
+        }
+
+        if (!blockStart) {
+            alert('Could not find starting element of type ' + type + '!');
+            break;
+        }
+
+        newBlock = document.createElement('DIV');
+        newBlock.className = 'ie' + type.toLowerCase();
+        if (blockStart.className.length) {
+            newBlock.className += ' ' + blockStart.className;
+        }
+
+        while (blockStart.nextSibling != blockEnd) {
+            newBlock.appendChild(blockStart.nextSibling);
+        }
+
+        blockEnd.parentNode.insertBefore(newBlock, blockStart);
+        newBlock.parentNode.removeChild(blockStart);
+        newBlock.parentNode.removeChild(blockEnd);
+    }
+
+    elems = document.getElementsByTagName(type);
+    while (elems.length) {
+        blockStart = elems[0];
+        if (blockStart.childNodes.length > 1) {
+            break;
+        }
+
+        blockEnd = blockStart.nextSibling;
+        while (blockEnd) {
+            if (blockEnd.nodeType == 8 && blockEnd.data.indexOf('/' + type.toLowerCase()) > -1) {
+                break;
+            } else {
+                blockEnd = blockEnd.nextSibling;
+            }
+        }
+
+        if (!blockEnd) {
+            break;
+        }
+
+        newBlock = document.createElement('DIV');
+        newBlock.className = 'ie' + type.toLowerCase();
+        if (blockStart.className.length) {
+            newBlock.className += ' ' + blockStart.className;
+        }
+
+        while (blockStart.nextSibling != blockEnd) {
+            newBlock.appendChild(blockStart.nextSibling);
+        }
+
+        blockEnd.parentNode.insertBefore(newBlock, blockStart);
+        newBlock.parentNode.removeChild(blockStart);
+        newBlock.parentNode.removeChild(blockEnd);
+    }
+}
+
+window.onload = function() {
+    fixElementsOfType('HEADER');
+    fixElementsOfType('MAIN');
+    fixElementsOfType('ARTICLE');
+    fixElementsOfType('SECTION');
+    fixElementsOfType('FOOTER');
+};
